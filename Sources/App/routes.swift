0af72e6b5aa106e2191702f9de6ea9db7e61eb94
Daniel Jilg <winsmith@winsmith.de>
2020-07-29 21:23:18 +0200

    Use leaf for templates

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,9 +1,26 @@
 import Fluent
 import Vapor
 
+struct Index: Codable {
+    var title: String
+    var description: String
+}
+
+struct Page: Codable {
+    var content: String
+}
+
+struct Context: Codable {
+    var index: Index
+    var page: Page
+}
+
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+    app.get { req -> EventLoopFuture<View> in
+        let context = Context(index: .init(title: "My page", description: "This is my Page"),
+                                      page: .init(content: "Welcome to my page!"))
+
+                return req.view.render("page", context)
     }
 
     app.get("hello") { req -> String in
