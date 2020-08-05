f67d1d02691f919f9466e4228ca379615f975f77
Daniel Jilg <winsmith@winsmith.de>
2020-08-05 08:08:50 +0200

    Telemetry Viewer can login now

---
+++ Telemetry Viewer/Shared/RegisterView.swift
@@ -0,0 +1,125 @@
+//
+//  RegisterView.swift
+//  Telemetry Viewer
+//
+//  Created by Daniel Jilg on 05.08.20.
+//
+
+import SwiftUI
+
+struct RegistrationRequestBody: Codable {
+    var organisationName: String = ""
+    var userFirstName: String = ""
+    var userLastName: String = ""
+    var userEmail: String = ""
+    var userPassword: String = ""
+    var userPasswordConfirm: String = ""
+}
+
+struct LoginRequestBody {
+    var userEmail: String = ""
+    var userPassword: String = ""
+    
+    var basicHTMLAuthString: String? {
+        let loginString = "\(userEmail):\(userPassword)"
+        guard let loginData = loginString.data(using: String.Encoding.utf8) else { return nil }
+        let base64LoginString = loginData.base64EncodedString()
+        return "Basic \(base64LoginString)"
+    }
+}
+
+struct UserToken: Codable {
+    var id: UUID?
+    var value: String
+    var user: String
+}
+
+struct RegisterView: View {
+    @State private var isLoading = false
+    @State private var shouldLogin = false
+    @State private var registrationRequestBody = RegistrationRequestBody()
+    @State private var loginRequestBody = LoginRequestBody()
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Login")) {
+                Toggle("Login instead of register?", isOn: $shouldLogin)
+            }
+            
+            if !shouldLogin {
+                Section(header: Text("Your Organization")) {
+                    TextField("Organization Name", text: $registrationRequestBody.organisationName)
+                }
+                
+                Section(header: Text("You")) {
+                    TextField("First Name", text: $registrationRequestBody.userFirstName)
+                    TextField("Last Name", text: $registrationRequestBody.userLastName)
+                    TextField("Email", text: $registrationRequestBody.userEmail)
+                }
+                
+                Section(header: Text("Your Password")) {
+                    TextField("Password", text: $registrationRequestBody.userPassword)
+                    TextField("Confirm Password", text: $registrationRequestBody.userPasswordConfirm)
+                }
+                
+                Section {
+                    if isLoading {
+                        ProgressView()
+                    } else {
+                        Button("Register") {
+                            print("\(registrationRequestBody)")
+                            isLoading = true
+                        }
+                    }
+                }
+            }
+            
+            if shouldLogin {
+                Section(header: Text("Login")) {
+                    TextField("Email", text: $loginRequestBody.userEmail)
+                    TextField("Password", text: $loginRequestBody.userPassword)
+                }
+                
+                Section {
+                    if isLoading {
+                        ProgressView()
+                    } else {
+                        Button("Login") {
+                            isLoading = true
+                            
+                            guard let url = URL(string: "http://localhost:8080/api/v1/users/login") else {
+                                print("Invalid URL")
+                                return
+                            }
+                            var request = URLRequest(url: url)
+                            request.httpMethod = "POST"
+                            request.setValue(loginRequestBody.basicHTMLAuthString, forHTTPHeaderField: "Authorization")
+                            
+                            URLSession.shared.dataTask(with: request) { data, response, error in
+                                isLoading = false
+                                
+                                if let data = data {
+                                    print(String(decoding: data, as: UTF8.self))
+                                    
+                                    if let decodedResponse = try? JSONDecoder().decode(UserToken.self, from: data) {
+                                        print(decodedResponse)
+                                    }
+                                }
+                            }.resume()
+                        }
+                    }
+                    
+                    
+                }
+            }
+        }.disabled(isLoading)
+        
+        
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
---
+++ Telemetry Viewer/Shared/Telemetry_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Telemetry_ViewerApp.swift
+//  Shared
+//
+//  Created by Daniel Jilg on 30.07.20.
+//
+
+import SwiftUI
+
+@main
+struct Telemetry_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
